941cd5658b9fb4578099a95207d7bbe334f0205b
Nader <Nader.Hassouni@Hotmail.com>
2020-07-28 01:56:05 +0200

    Replaced favicon,page title and added carousel to gallery

--- frontend/src/home/components/Gallery.js
+++ frontend/src/home/components/Gallery.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
 import "./Gallery.css";
 
 import useHttpRequest from "../../shared/hooks/http-hook";
 import ErrorModal from "../../shared/components/UIElements/Modal/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
-import GalleryItem from "./GalleryItem";
-import EmptyGallery from "./EmptyGallery";
 
 const Gallery = () => {
   const [galleryData, setGalleryData] = useState([]);
@@ -17,19 +18,10 @@ const Gallery = () => {
     const url = "/api/places";
     try {
       const responseData = await sendRequest(url);
-
-      getRandomElements(responseData, 4);
+      setGalleryData(responseData);
     } catch (err) {
       console.log("Error in fetching places!", err);
     }
-  };
-
-  const getRandomElements = (sourceArray, neededElements) => {
-    let result = [];
-    for (let i = 0; i < neededElements; i++) {
-      result.push(sourceArray[Math.floor(Math.random() * sourceArray.length)]);
-    }
-    setGalleryData(result);
   };
 
   useEffect(() => {
@@ -44,13 +36,15 @@ const Gallery = () => {
 
       {isLoading && <LoadingSpinner asOverlay />}
 
-      {!isLoading && galleryData.length > 0 ? (
+      <Carousel showArrows={true} infiniteLoop={true} showStatus={false} showThumbs={false} autoPlay={true} className='carousel' >
+      {!isLoading && galleryData.length > 0 &&
         galleryData.map((place, i) => (
-          <GalleryItem key={i} image={place.image} title={place.title} />
-        ))
-      ) : (
-        <EmptyGallery />
-      )}
+          <div>
+          <img src={place.image} />
+          <p className="legend">{place.title}</p>
+      </div>
+        ))}
+          </Carousel>
     </div>
   );
 };
