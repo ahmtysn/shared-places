31707fc37ec6fe35b34482e95f1cca1a44ea6de5
M Rasho <m.rasho90@gmail.com>
2020-07-31 01:25:17 +0200

    diplay date and time for each news feed item

--- frontend/src/users/components/PlacesFeedUI.js
+++ frontend/src/users/components/PlacesFeedUI.js
@@ -7,6 +7,16 @@ import useAuth from "../../shared/hooks/auth-hook";
 import '../../places/components/PlaceItem.css'
 import { Link } from 'react-router-dom';
 const PlacesFeedUI = ({ news }) => {
+  let today = new Date();
+  let splittingDate = news.date.split('-');
+  let splittingTime = news.time.split(':');
+  let year = today.getFullYear() - splittingDate[0]
+  let month = today.getMonth() + 1 - splittingDate[1]
+  let day = today.getDate() - splittingDate[2]
+  let hours = Math.abs(today.getHours() - splittingTime[0])
+  let minutes = Math.abs(today.getMinutes() - splittingTime[1])
+  
+
   const { userId } = useAuth();
   const [p, setP] = useState();
   const { isLoading, error, clearError, sendRequest } = useHttpRequest();
@@ -28,6 +38,7 @@ const PlacesFeedUI = ({ news }) => {
     {isLoading && <LoadingSpinner asOverlay />}
     {!isLoading && p && <Feed>
       <Feed.Event>
+        {console.log(p.title, year, month, day, hours)}
         <Feed.Label>
           <Image
             src={`http://localhost:5000/${p.creator.image}`}
@@ -36,7 +47,26 @@ const PlacesFeedUI = ({ news }) => {
         <Feed.Content>
           <Feed.Summary>
             <Link>{p.creator.id === userId ? "You" : p.creator.name}</Link> added a new place <Link> {p.title}</Link>
-            <Feed.Date>{news.date}</Feed.Date>
+            <Feed.Date>{
+              year === 0 && month === 0 && day === 0 && hours === 0 && minutes === 0 ? `Few seconds Ago`
+                :
+                year === 0 && month === 0 && day === 0 && hours === 0 && minutes < 1 ? `${minutes} minute Ago`
+                  :
+                  year === 0 && month === 0 && day === 0 && hours === 0 ? `${minutes} minutes Ago`
+                    :
+                    hours === 1 && day === 0 ? `${hours} Hour Ago`
+                      :
+                      hours < 24 && day === 0 ? `${hours} Hours Ago`
+                        :
+                        month === 0 && day === 1 ? `${day} day Ago`
+                          :
+                          month === 0 && day > 1 ? `${day} days Ago`
+                            :
+                            month < 12 ? `${month} Months Ago`
+                              :
+                              'Over than Year ago'
+
+            }</Feed.Date>
           </Feed.Summary>
           <Feed.Extra images>
             <div>
