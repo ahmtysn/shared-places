e3e4961c04a23e6d795bc4919daa2601ab4636ba
okayns <okayenis@gmail.com>
2020-09-05 23:31:14 +0200

    new dev database added

--- util/connectDB.js
+++ util/connectDB.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const DB_USER = process.env.DB_USER;
 const DB_PW = process.env.DB_PW;
 const DB_NAME = process.env.DB_NAME;
-const DB_URL = `mongodb+srv://${DB_USER}:${DB_PW}@cluster0-jtuqz.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-const DB_URL1 = `mongodb+srv://manu:5jltYkJttJkaxXBg@cluster0-c0hbd.mongodb.net/mern?retryWrites=true&w=majority`;
+const DB_URL = `mongodb+srv://${DB_USER}:${DB_PW}@cluster0.fxfqv.azure.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
 const connectDB = (server) => {
   mongoose
-    .connect(DB_URL1, {
+    .connect(DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
@@ -17,8 +17,5 @@ const connectDB = (server) => {
     .then(() => server())
     .catch((err) => console.log('err happened in db connection!', err));
 };
-// the logic of connecting the backend to mongoose is like:
-// 1- Establish the connecting to database
-// 2- connection successful? then we start our backend server. (Not successful then we don't need server because we will throw an error )
 
 module.exports = connectDB;
