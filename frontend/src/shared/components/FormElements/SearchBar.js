ab76c6447692dbf4df57aae6a88e84bece51536d
Naji <najin546@gmail.com>
2020-09-10 14:47:54 +0200

    mobile version

--- frontend/src/shared/components/FormElements/SearchBar.js
+++ frontend/src/shared/components/FormElements/SearchBar.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import SearchIcon from '@material-ui/icons/Search';
-import { makeStyles } from '@material-ui/core/styles';
-import { Container, Paper, InputBase, IconButton } from '@material-ui/core';
+import React from "react";
+import "./SearchBar.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Paper, InputBase, IconButton } from "@material-ui/core";
 const useStyles = makeStyles((theme) => ({
   root: {
-    padding: '2px 4px',
-    display: 'flex',
-    alignItems: 'center',
-    width: '50%',
-    margin: 'auto',
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+    width: "100%",
+    height: "auto",
+    margin: "auto",
   },
   input: {
     marginLeft: theme.spacing(1),
@@ -27,28 +29,30 @@ const SearchBar = ({
 }) => {
   const classes = useStyles();
   return (
-    <Container>
-      <Paper
-        component='form'
-        className={classes.root}
-        onSubmit={onSubmitSearchHandler}
-      >
-        <InputBase
-          className={classes.input}
-          placeholder={placeholder}
-          inputProps={{ 'aria-label': '' }}
-          value={searchValue}
-          onChange={inputSearchHandler}
-        />
-        <IconButton
-          type='submit'
-          className={classes.iconButton}
-          aria-label='search'
+    <div className="SearchBarContainer">
+      <Container>
+        <Paper
+          component="form"
+          className={classes.root}
+          onSubmit={onSubmitSearchHandler}
         >
-          <SearchIcon />
-        </IconButton>
-      </Paper>
-    </Container>
+          <InputBase
+            className={classes.input}
+            placeholder={placeholder}
+            inputProps={{ "aria-label": "" }}
+            value={searchValue}
+            onChange={inputSearchHandler}
+          />
+          <IconButton
+            type="submit"
+            className={classes.iconButton}
+            aria-label="search"
+          >
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+      </Container>
+    </div>
   );
 };
 
